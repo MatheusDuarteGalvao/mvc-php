3eb10701fa796ecdbbf5f91af58347aedbd3263e
Matheus Duarte <matheusduartegalvao@gmail.com>
2021-05-11 20:56:44 -0300

    Ajustes dos usuarios

---
+++ app/Controller/Admin/User.php
@@ -0,0 +1,230 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use \App\Utils\View;
+use \App\Model\Entity\User as EntityUser;
+use \WilliamCosta\DatabaseManager\Pagination;
+
+class User extends Page{
+
+    /**
+     * Método responsável por obter a renderização dos items de usuários para a página
+     * @param Request $request
+     * @param Pagination $obPagination
+     * @return string
+    */
+    private static function getUserItems($request,&$obPagination){
+        //DEPOIMENTOS
+        $items = '';
+
+        //QUANTIDADE TOTAL DE REGISTROS
+        $quantidadeTotal = EntityUser::getUsers(null, null,null,'COUNT(*) as qtd')->fetchObject()->qtd;
+
+        //PÁGINA ATUAL
+        $queryParams = $request->getQueryParams();
+        $paginaAtual = $queryParams['page'] ?? 1;
+
+        //INSTÂNCIA DE PAGINAÇÃO
+        $obPagination = new Pagination($quantidadeTotal,$paginaAtual,5);
+
+        //RESULTADOS DA PÁGINA
+        $results = EntityUser::getUsers(null,'id DESC',$obPagination->getLimit());
+
+        //RENDERIZA O ITEM
+        while($obUser = $results->fetchObject(EntityUser::class)){
+            $items .= View::render('admin/modules/users/item',[
+                'id'        => $obUser->id,
+                'nome'      => $obUser->nome,
+                'email'     => $obUser->email,
+            ]);
+        }
+
+        //RETORNA OS DEPOIMENTOS
+        return $items;
+    }
+
+    /**
+     * Método responsável por por renderizar a view de listagem de usuários
+     * @param Request $request
+     * @return string
+     */
+    public static function getUsers($request){
+        //CONTEÚDO DA HOME
+        $content = View::render('admin/modules/users/index',[
+            'items'      => self::getUserItems($request,$obPagination),
+            'pagination' => parent::getPagination($request,$obPagination),
+            'status'     => self::getStatus($request)
+        ]);
+
+        //RETORNA A PÁGINA COMPLETA
+        return parent::getPanel('Usuários > Admin',$content, 'users');
+    }
+
+    /**
+     * Método responsável por retornar o formulário de cadastro de um novo usuário
+     * @param Request $request
+     * @return string
+     */
+    public static function getNewUser($request){
+        //CONTEÚDO DO FORMULÁRIO
+        $content = View::render('admin/modules/users/form',[
+            'title'     => 'Cadastrar usuário',
+            'nome'      => '',
+            'email'     => '',
+            'senha'     => '',
+            'status'    => ''
+        ]);
+
+        //RETORNA A PÁGINA COMPLETA
+        return parent::getPanel('Cadastrar usuário > Admin',$content, 'users');
+    }
+
+    /**
+     * Método responsável por cadastrar um usuário no banco
+     * @param Request $request
+     * @return string
+     */
+    public static function setNewUser($request){
+        //POST VARS
+        $postVars = $request->getPostVars();
+
+        //NOVA INSTÂNCIA DE DEPOIMENTO
+        $obUser         = new EntityUser;
+        $obUser->nome   = $postVars['nome'] ?? '';
+        $obUser->email  = $postVars['email'] ?? '';
+        $obUser->senha  = $postVars['senha'] ?? '';
+        $obUser->cadastrar();
+
+        //REDIRECIONA O USUÁRIO
+        $request->getRouter()->redirect('/admin/users/'.$obUser->id.'/edit?status=created');
+    }
+
+    /**
+     * Método responsável por retornar o formulário de edição
+     * @param Request $request
+     * @return string
+     */
+    private static function getStatus($request){
+        //QUERY PARAMS
+        $queryParams = $request->getQueryParams();
+
+        //STATUS
+        if(!isset($queryParams['status'])) return '';
+
+        //MENSAGENS DE STATUS
+        switch ($queryParams['status']) {
+            case 'created':
+                return Alert::getSuccess('Usuário criado com sucesso!');
+                break;
+            case 'updated':
+                return Alert::getSuccess('Usuário atualizado com sucesso!');
+                break;
+            case 'deleted':
+                return Alert::getSuccess('Usuário excluído com sucesso!');
+                break;
+        }
+    }
+
+    /**
+     * Método responsável por retornar o formulário de edição de um usuário
+     * @param Request $request
+     * @param integer $id
+     * @return string
+     */
+    public static function getEditUser($request,$id){
+        //OBTÉM O DEPOIMENTO DO BANCO DE DADOS
+        $obUser = EntityUser::getUserById($id);
+
+        //VALIDA A INSTÂNCIA
+        if(!$obUser instanceof EntityUser){
+            $request->getRouter()->redirect('/admin/users');
+        }
+
+        //CONTEÚDO DO FORMULÁRIO
+        $content = View::render('admin/modules/users/form',[
+            'title'     => 'Editar usuário',
+            'nome'      => $obUser->nome,
+            'email'     => $obUser->email,
+            'status'    => self::getStatus($request)
+        ]);
+
+        //RETORNA A PÁGINA COMPLETA
+        return parent::getPanel('Editar usuário > Admin',$content, 'users');
+    }
+
+    /**
+     * Método responsável por gravar a atualização de um usuário
+     * @param Request $request
+     * @param integer $id
+     * @return string
+     */
+    public static function setEditUser($request,$id){
+        //OBTÉM O DEPOIMENTO DO BANCO DE DADOS
+        $obUser = EntityUser::getUserById($id);
+
+        //VALIDA A INSTÂNCIA
+        if(!$obUser instanceof EntityUser){
+            $request->getRouter()->redirect('/admin/users');
+        }
+
+        //POST VARS
+        $postVars = $request->getPostVars();
+
+        //ATUALIZA A INSTÂNCIA
+        $obUser->nome    = $postVars['nome'] ?? $obUser->nome;
+        $obUser->email   = $postVars['email'] ?? $obUser->email;
+        $obUser->senha   = $postVars['senha'] ?? '';
+        $obUser->atualizar();
+
+        //REDIRECIONA O USUÁRIO
+        $request->getRouter()->redirect('/admin/users/'.$obUser->id.'/edit?status=updated');
+    }
+
+    /**
+     * Método responsável por retornar o formulário de exclusão de um usuário
+     * @param Request $request
+     * @param integer $id
+     * @return string
+     */
+    public static function getDeleteUser($request,$id){
+        //OBTÉM O DEPOIMENTO DO BANCO DE DADOS
+        $obUser = EntityUser::getUserById($id);
+
+        //VALIDA A INSTÂNCIA
+        if(!$obUser instanceof EntityUser){
+            $request->getRouter()->redirect('/admin/users');
+        }
+
+        //CONTEÚDO DO FORMULÁRIO
+        $content = View::render('admin/modules/users/delete',[
+            'nome'      => $obUser->nome,
+            'mensagem'  => $obUser->mensagem
+        ]);
+
+        //RETORNA A PÁGINA COMPLETA
+        return parent::getPanel('Excluir usuário > Admin',$content, 'users');
+    }
+
+    /**
+     * Método responsável excluir um usuário
+     * @param Request $request
+     * @param integer $id
+     * @return string
+     */
+    public static function setDeleteUser($request,$id){
+        //OBTÉM O DEPOIMENTO DO BANCO DE DADOS
+        $obUser = EntityUser::getUserById($id);
+
+        //VALIDA A INSTÂNCIA
+        if(!$obUser instanceof EntityUser){
+            $request->getRouter()->redirect('/admin/users');
+        }
+
+        //EXCLUI O DEPOIMENTO
+        $obUser->excluir();
+
+        //REDIRECIONA O USUÁRIO
+        $request->getRouter()->redirect('/admin/users?status=deleted');
+    }
+}
--- app/Model/Entity/User.php
+++ app/Model/Entity/User.php
@@ -38,4 +38,75 @@ class User{
     public static function getUserByEmail($email){
         return (new Database('usuarios'))->select('email = "'.$email.'"')->fetchObject(self::class);
     }
+
+    /**
+     * Método responsável por cadastrar a instância atual no banco de dados
+     * @return boolean
+    */
+    public function cadastrar(){
+        //DEFINE A DATA
+        $this->data = date('Y-m-d H:i:s');
+
+        //DEFINE A SENHA
+        $this->senha = password_hash($this->senha,PASSWORD_DEFAULT);
+
+        //INSERE O USUÁRIO NO BANCO DE DADOS
+        $this->id = (new Database('usuarios'))->insert([
+            'nome'  => $this->nome,
+            'email' => $this->email,
+            'senha' => $this->senha
+        ]);
+
+        //SUCESSO
+        return true;
+    }
+
+    /**
+     * Método responsável por atualizar os dados do banco com a instância atual
+     * @return boolean
+    */
+    public function atualizar(){
+        //DEFINE A SENHA
+        $this->senha = password_hash($this->senha,PASSWORD_DEFAULT);
+
+        //ATUALIZA O USUÁRIO NO BANCO DE DADOS
+        return (new Database('usuarios'))->update('id = '.$this->id,[
+            'nome'      => $this->nome,
+            'mensagem'  => $this->mensagem,
+            'senha'     => $this->senha
+        ]);
+
+        //SUCESSO
+        return true;
+    }
+
+    /**
+     * Método responsável por excluir um usuário do banco de dados
+     * @return boolean
+    */
+    public function excluir(){
+        //EXCLUI O USUÁRIO NO BANCO DE DADOS
+        return (new Database('usuarios'))->delete('id = '.$this->id);
+    }
+
+    /**
+     * Método responsável por retornar usuários
+     * @param string $where
+     * @param string $order
+     * @param string $limit
+     * @param string $field
+     * @return PDOStatement
+    */
+    public static function getUsers($where = null, $order = null, $limit = null, $fields = '*'){
+        return (new Database('usuarios'))->select($where,$order,$limit,$fields);
+    }
+
+    /**
+     * Método responsável por retornar um usuário com base no seu ID
+     * @param integer $id
+     * @return User
+     */
+    public static function getUserById($id){
+        return self::getUsers('id = '.$id)->fetchObject(self::class);
+    }
 }
